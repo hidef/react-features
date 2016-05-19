64c6687a6abc39f05e3a1fe943765c5b0defec8a
Robert Stiff <uatecuk@gmail.com>
2016-05-19 08:20:56 +0100

    Proof of ES6 and JSX build capability

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+// Validate react
+var thing = <div>
+        Hello World
+    </div>;
+
+export { thing };
---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+    target: 'web',
+    entry: './src/index.js',
+    output: {
+        path: './dist',
+        filename: 'index.js'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                loader: 'jsx-loader?harmony!babel'
+            },
+            {
+                test: /\.css$/,
+                loader: "style-loader!css-loader"
+            }, {
+                test: /\.less$/,
+                loader: "style!css!less"
+            },
+            { test: /\.(png|jpg)$/, loader: 'url-loader?limit=8192' }, // inline base64 URLs for <=8k images, direct URLs for the rest
+            { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "url-loader?limit=10000&minetype=application/font-woff" },
+            { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "file-loader" },
+            {
+                test: /\.json$/,
+                loader: 'json-loader'
+            }
+        ]
+    }
+};
