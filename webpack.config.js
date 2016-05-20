3252556820da5639184cd0c6c3458462f4e1f065
Robert Stiff <uatecuk@gmail.com>
2016-05-20 08:46:03 +0100

    Tidy up webpack conf lint issues.

--- webpack.config.js
+++ webpack.config.js
@@ -3,28 +3,34 @@ module.exports = {
     entry: './src/index.js',
     output: {
         path: './dist',
-        publicPath: "/dist/",
-        filename: 'index.js'
+        publicPath: '/dist/',
+        filename: 'index.js',
     },
     module: {
         loaders: [
             {
                 test: /\.(js|jsx)/,
-                loader: 'jsx-loader?harmony!babel!eslint-loader'
+                loader: 'jsx-loader?harmony!babel!eslint-loader',
             },
             {
                 test: /\.css$/,
-                loader: "style-loader!css-loader"
+                loader: 'style-loader!css-loader',
             }, {
                 test: /\.less$/,
-                loader: "style!css!less"
-            },
-            { test: /\.(png|jpg)$/, loader: 'url-loader?limit=8192' }, // inline base64 URLs for <=8k images, direct URLs for the rest
-            { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "url-loader?limit=10000&minetype=application/font-woff" },
-            { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "file-loader" },
+                loader: 'style!css!less',
+            }, {
+                test: /\.(png|jpg)$/,
+                loader: 'url-loader?limit=8192',
+            }, // inline base64 URLs for <=8k images, direct URLs for the rest
             {
+                test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+                loader: 'url-loader?limit=10000&minetype=application/font-woff',
+            }, {
+                test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+                loader: 'file-loader',
+            }, {
                 test: /\.json$/,
-                loader: 'json-loader'
+                loader: 'json-loader',
             }
         ]
     }
