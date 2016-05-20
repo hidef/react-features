5b8bf74dcd9780c1706d34110269a4785ca21bc3
Robert Stiff <uatecuk@gmail.com>
2016-05-20 08:33:58 +0100

    eslint niceness for tests aswell.

--- test/feature.test.js
+++ test/feature.test.js
@@ -1,52 +1,53 @@
 import React from 'react';
 import { expect } from 'chai';
-import { shallow, mount, render } from 'enzyme';
+import { render } from 'enzyme';
 import Feature from '../src/components/feature.jsx';
 import FeatureContainer from '../src/components/featureContainer.jsx';
 
 
-const PropsPrinter = (props, context) => <div>{props[props.propsKey]}</div>;
+const PropsPrinter = (props) => <div>{props[props.propsKey]}</div>;
+PropsPrinter.propTypes = { propsKey: React.PropTypes.string.isRequired };
 
-describe("A feature", function () {
-  describe("When a feature key is defined in the `features` context element", function () {
-    it('it will render the features children', function () {
-      var rendered = render(<FeatureContainer features={{ foo: true }}>
-        <Feature featureKey="foo">
-          <div>
-            hello world
-          </div>
-        </Feature>
-      </FeatureContainer>);
-      expect(rendered.text()).to.contain('hello world');
-    });
-    
-    it('the children will receive the value of the feature as a prop', function () {
-      var rendered = render(<FeatureContainer features={{ foo: 'my feature config value' }}>
-        <Feature featureKey="foo">
-          <PropsPrinter propsKey='featureConfig' />
-        </Feature>
-      </FeatureContainer>);
-      expect(rendered.text()).to.contain('my feature config value');
-    });
-  });
+describe('A feature', () => {
+    describe('When a feature key is defined in the `features` context element', () => {
+        it('it will render the features children', () => {
+            const rendered = render(<FeatureContainer features={{ foo: true }}>
+              <Feature featureKey="foo">
+                <div>
+                  hello world
+                </div>
+              </Feature>
+            </FeatureContainer>);
+            expect(rendered.text()).to.contain('hello world');
+        });
 
-  describe("When a feature key is undefined in the `features` context element", function () {
-    it('it will not render the features children', function () {
-      expect(render(<FeatureContainer features={{}}>
-        <Feature featureKey="foo">
-          <div className="foo" />
-        </Feature>
-      </FeatureContainer>).find(<div className="foo" />)).to.have.length(0);
+        it('the children will receive the value of the feature as a prop', () => {
+            const rendered = render(<FeatureContainer features={{ foo: 'my feature config value' }}>
+              <Feature featureKey="foo">
+                <PropsPrinter propsKey="featureConfig" />
+              </Feature>
+            </FeatureContainer>);
+            expect(rendered.text()).to.contain('my feature config value');
+        });
     });
-  });
+
+    describe('When a feature key is undefined in the `features` context element', () => {
+        it('it will not render the features children', () => {
+            expect(render(<FeatureContainer features={{}}>
+              <Feature featureKey="foo">
+                <div className="foo" />
+              </Feature>
+            </FeatureContainer>).find(<div className="foo" />)).to.have.length(0);
+        });
+    });
 });
 
-describe("When a feature key is false in the `features` context element", function () {
-  it('it will not render the features children', function () {
-    expect(render(<FeatureContainer features={{ foo: false }}>
-      <Feature featureKey="foo">
-        <div className="foo" />
-      </Feature>
-    </FeatureContainer>).find(<div className="foo" />)).to.have.length(0);
-  });
+describe('When a feature key is false in the `features` context element', () => {
+    it('it will not render the features children', () => {
+        expect(render(<FeatureContainer features={{ foo: false }}>
+          <Feature featureKey="foo">
+            <div className="foo" />
+          </Feature>
+        </FeatureContainer>).find(<div className="foo" />)).to.have.length(0);
+    });
 });
--- test/featureContainer.test.js
+++ test/featureContainer.test.js
@@ -1,27 +1,28 @@
 import React from 'react';
 import { expect } from 'chai';
-import { shallow, mount, render } from 'enzyme';
+import { shallow, render } from 'enzyme';
 import FeatureContainer from '../src/components/featureContainer.jsx';
 
 
 const ContextPrinter = (props, context) => <div>{JSON.stringify(context.features)}</div>;
 
 ContextPrinter.contextTypes = {
-  features: React.PropTypes.object
+    features: React.PropTypes.object,
 };
 
-describe("A feature container", function () {
-  it("renders its children", function () {
-    expect(shallow(<FeatureContainer>
-      <div className="foo" />
-    </FeatureContainer>).contains(<div className="foo" />)).to.equal(true);
-  });
-  
-  it('will exposes the features prop on context to children', function() {
-    var features = {a: true, b: false};
-    var element = render(<FeatureContainer features={features}>
-        <ContextPrinter />
-      </FeatureContainer>);
-      expect(element[0].children[0].children[0].children[0].data).to.equal(JSON.stringify(features));
-  });
+describe('A feature container', () => {
+    it('renders its children', () => {
+        expect(shallow(<FeatureContainer>
+          <div className="foo" />
+        </FeatureContainer>).contains(<div className="foo" />)).to.equal(true);
+    });
+
+    it('will exposes the features prop on context to children', () => {
+        const features = { a: true, b: false };
+        const element = render(<FeatureContainer features={features}>
+          <ContextPrinter />
+        </FeatureContainer>);
+        expect(element[0].children[0].children[0].children[0].data)
+            .to.equal(JSON.stringify(features));
+    });
 });
